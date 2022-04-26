58dfa7199d155b2e11e9ebeaf859df035b904ded
caiqueb05 <caique.bezerra@gmail.com>
2022-04-26 15:18:41 -0300

    @caiqueb05 [Task: adicionados componentes menu, navbar e painel]

--- desafio-cerc/src/app/app.module.ts
+++ desafio-cerc/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { MenuLatEsqComponent } from './menu-lat-esq/menu-lat-esq.component';
 import { PainelComponent } from './painel/painel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PainelComponent } from './painel/painel.component';
     CadastrarComponent,
     EntrarComponent,
     MenuLatEsqComponent,
-    PainelComponent
+    PainelComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
